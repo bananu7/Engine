6c89e239195e6d2874fc397cb80a6d846b5fe700
Bartek Banachewicz <bananu7@o2.pl>
2013-03-14 00:47:07 +0100

    Fixed potential memory leaks in Image loader.

--- Engine/src/Image.cpp
+++ Engine/src/Image.cpp
@@ -2,6 +2,7 @@
 #include <GL/glew.h>
 #include "Misc.h"
 #include <vector>
+#include <memory>
 #include <iostream>
 #include <iterator>
 
@@ -13,54 +14,68 @@ namespace engine {
 using std::string;
 using std::runtime_error;
 
+struct FreeImageMemoryDeleter {
+	void operator()(FIMEMORY* mem) {
+		FreeImage_CloseMemory(mem);
+	}
+};
+
+struct FreeImageBitmapDeleter {
+	void operator()(FIBITMAP* dib) {
+		FreeImage_Unload(dib);
+	}
+};
+
+template<typename T, typename Deleter>
+inline std::unique_ptr<T, Deleter> make_unique_raw(T* t, Deleter&& d) {
+	return std::unique_ptr<T, Deleter>(t, std::forward<Deleter>(d));
+}
+
 Image Image::_internalLoad(std::vector<unsigned char>&& vd)
 {
 	Image temporary;
-
-	//image format
-	FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
-	//pointer to the image, once loaded
-	FIBITMAP *dib(0);
-	//pointer to the image data
-	BYTE* bits(0);
-	//image width and height
-	unsigned int width(0), height(0);
 	
 	// FIXME
 	GLenum internal_format = GL_RGBA;
 	GLuint image_format = GL_BGRA;
 	GLuint level = 0;
 	GLuint border = 0;
-
-	auto fm = FreeImage_OpenMemory(vd.data(), vd.size());
+	
+	auto fm = make_unique_raw(FreeImage_OpenMemory(vd.data(), vd.size()), FreeImageMemoryDeleter());
 
 	//check the file signature and deduce its format
-	fif = FreeImage_GetFileTypeFromMemory(fm, vd.size());
-	//fif = FreeImage_GetFileType(path);
+	FREE_IMAGE_FORMAT fif = FreeImage_GetFileTypeFromMemory(fm.get(), vd.size());
 
 	//if still unknown, try to guess the file format from the file extension
 	//if(fif == FIF_UNKNOWN) 
 	//	fif = FreeImage_GetFIFFromFilename(filename);
 	//if still unkown, return failure
 	if(fif == FIF_UNKNOWN)
-		throw runtime_error("Failed to deduce filetype");
+		throw runtime_error("Failed to deduce image filetype");
 
 	//check that the plugin has reading capabilities and load the file
-	if(FreeImage_FIFSupportsReading(fif))
-		dib = FreeImage_LoadFromMemory(fif, fm);
+	if(!FreeImage_FIFSupportsReading(fif))
+		throw runtime_error("This image type is not supported");
+
+	//auto dib = std::unique_ptr<FIBITMAP, FreeImageBitmapDeleter>(
+	//	FreeImage_LoadFromMemory(fif, fm.get()), FreeImageBitmapDeleter());
+
+	auto dib = make_unique_raw(FreeImage_LoadFromMemory(fif, fm.get()), FreeImageBitmapDeleter());
+
 	//	dib = FreeImage_Load(fif, path);
+
 	//if the image failed to load, return failure
 	if(!dib)
 		throw runtime_error("Image failed to load");
 
-	FreeImage_FlipVertical(dib); 
-	dib = FreeImage_ConvertTo32Bits(dib);
+	FreeImage_FlipVertical(dib.get()); 
+	dib.reset(FreeImage_ConvertTo32Bits(dib.get()));
 
-	//retrieve the image data
-	bits = FreeImage_GetBits(dib);
+	//raw pointer to the image data
+	BYTE* bits = FreeImage_GetBits(dib.get());
 	//get the image width and height
-	width = FreeImage_GetWidth(dib);
-	height = FreeImage_GetHeight(dib);
+	unsigned width = FreeImage_GetWidth(dib.get());
+	unsigned height = FreeImage_GetHeight(dib.get());
 	//if this somehow one of these failed (they shouldn't), return failure
 	if((bits == 0) || (width == 0) || (height == 0))
 		throw runtime_error("Image checking failed");
@@ -72,11 +87,6 @@ Image Image::_internalLoad(std::vector<unsigned char>&& vd)
 	//store the texture data for OpenGL use
 	glTexImage2D(GL_TEXTURE_2D, level, internal_format, width, height,
 		border, image_format, GL_UNSIGNED_BYTE, bits);
-
-	//Free FreeImage's copy of the data
-	FreeImage_Unload(dib);
-	//Free FreeImage's memory stream object
-	FreeImage_CloseMemory(fm);
 	
 	//return success
 	return std::move(temporary);
@@ -113,6 +123,7 @@ Image::Image(Image&& other) {
 Image& Image::operator= (Image&& other) {
 	m_TexId = other.m_TexId;
 	other.m_TexId = 0;
+	return *this;
 }
 
 
